8ab1bd0da895b0c836c1ab278f37dbedb07ec9ac
Oskar Augustynek <oskar.augustynek@gmail.com>
2023-12-11 19:50:03 +0100

    Method for getting floats and scientific notations from list

--- src/main/java/NumberChooser.java
+++ src/main/java/NumberChooser.java
@@ -7,12 +7,25 @@ public class NumberChooser {
     public static void main(String[] args) {
         String[] numbers = {"342", "5.34", "756","1.234e+07", "7.234243E-02", "6.09", "3457", "87", "1.0001", "3", "5"};
         List <String> numbersList = List.of(numbers);
-        showAllIntegers(numbersList);
-
+        showScientificNotations(numbersList);
     }
     public static void showAllIntegers (List<String> text){
         for (int i = 0; i <= text.size() - 1; i++){
             if (text.get(i).matches("([0-9]+)*")){
+                System.out.println(text.get(i));
+            }
+        }
+    }
+    public static void showFloats (List<String> text){
+        for (int i = 0; i <= text.size() - 1; i++){
+            if (text.get(i).matches("([0-9])*\\.([0-9]+)*")){
+                System.out.println(text.get(i));
+            }
+        }
+    }
+    public static void showScientificNotations (List<String> text){
+        for (int i = 0; i <= text.size() - 1; i++){
+            if (text.get(i).matches("([0-9]+)*.([0-9]+)*(e|E)([+-])([0-9]+)*")){
                 System.out.println(text.get(i));
             }
         }
