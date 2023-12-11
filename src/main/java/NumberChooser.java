501f61ca9f8413ce8940748147a5f3983c7a3d29
Oskar Augustynek <oskar.augustynek@gmail.com>
2023-12-11 02:43:06 +0100

    scientific notation regex

--- src/main/java/NumberChooser.java
+++ src/main/java/NumberChooser.java
@@ -7,16 +7,18 @@ public class NumberChooser {
 
         Pattern integersPattern = Pattern.compile("([0-9]+)*");
         Matcher integers = integersPattern.matcher("12123123");
-
         boolean matchesIntegers = integers.matches();
         System.out.println("Czy pasuje do liczb całkowitych: " + matchesIntegers);
 
 
         Pattern floatsPattern = Pattern.compile("([0-9]+)*.([0-9]+)*");
         Matcher floats = floatsPattern.matcher("244.008");
-
         boolean matchesFloats = floats.matches();
         System.out.println("Czy pasuje do liczb zmiennoprzecinkowych: " + matchesFloats);
 
+        Pattern scientificNotationPattern = Pattern.compile("([0-9]+)*.([0-9]+)*(e|E)([+-])([0-9]+)*");
+        Matcher scientificNotation = scientificNotationPattern.matcher("1.234e+07");
+        boolean matchesScientificNotation = scientificNotation.matches();
+        System.out.println("Czy pasuje do notacji naukowej: " + matchesScientificNotation);
     }
 }
