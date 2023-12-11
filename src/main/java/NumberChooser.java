558e8c61ef751f29e8102202e5f5a3ac0330d435
Oskar Augustynek <oskar.augustynek@gmail.com>
2023-12-11 02:27:42 +0100

    integers regex

--- src/main/java/NumberChooser.java
+++ src/main/java/NumberChooser.java
@@ -1,2 +1,15 @@
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 public class NumberChooser {
+    public static void main(String[] args) {
+
+
+        Pattern integersPattern = Pattern.compile("([0-9]+)*");
+        Matcher matcher = integersPattern.matcher("12123123");
+
+        boolean matches = matcher.matches();
+        System.out.println("Czy pasuje do liczb całkowitych: " + matches);
+
+    }
 }
