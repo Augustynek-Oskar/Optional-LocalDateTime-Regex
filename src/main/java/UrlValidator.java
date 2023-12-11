d0ccfc53567b0a16591a2fb38a005c73020d7d68
Oskar Augustynek <oskar.augustynek@gmail.com>
2023-12-11 18:34:59 +0100

    method for urlValidation

--- src/main/java/UrlValidator.java
+++ src/main/java/UrlValidator.java
@@ -2,24 +2,14 @@ import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class UrlValidator {
-    public static void main(String[] args) {
+    public boolean isUrlValid(String yourUrl){
         Pattern urlPattern = Pattern.compile("^(https|http)://[a-z.]*$");
-        Matcher onetLongUrl = urlPattern.matcher("http://www.onet.pl");
-        Matcher gmailUrl = urlPattern.matcher("https://mail.google.com");
-        Matcher onetNewsUrl = urlPattern.matcher("http://wiadmosci.onet.pl");
-        Matcher onetShortUrl = urlPattern.matcher("http://onet.pl");
-
-        boolean matchesOnetLongUrl = onetLongUrl.matches();
-        System.out.println(matchesOnetLongUrl);
-
-        boolean matchesGmailUrl = gmailUrl.matches();
-        System.out.println(matchesGmailUrl);
-
-        boolean matchesOnetNewsUrl = onetNewsUrl.matches();
-        System.out.println(matchesOnetNewsUrl);
-
-        boolean matchesOnetShortUrl = onetShortUrl.matches();
-        System.out.println(matchesOnetShortUrl);
-
+        Matcher url = urlPattern.matcher(yourUrl);
+        boolean matches = url.matches();
+        if (!matches){
+            System.out.println("Invalid url");
+            return false;
+        }
+        return true;
     }
 }
