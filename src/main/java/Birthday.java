44501657d3436c956acf4b1a0840140520332111
Oskar Augustynek <oskar.augustynek@gmail.com>
2023-12-14 17:53:45 +0100

    getAge method that counts how old person is

--- src/main/java/Birthday.java
+++ src/main/java/Birthday.java
@@ -1,9 +1,18 @@
 import java.time.LocalDate;
+import java.time.Month;
+import java.time.Period;
 
 public class Birthday {
+    public static void main(String[] args) {
+        getAge("2001-06-17");
+    }
 
-    public static void getAge(LocalDate dateOfBirth){
-
+    public static int getAge(String dateOfBirth){
+        LocalDate localDateNow = LocalDate.now();
+        LocalDate birthDate = LocalDate.parse(dateOfBirth);
+        Period period = Period.between(birthDate, localDateNow);
+        System.out.println(localDateNow.getYear() - birthDate.getYear());
+        return localDateNow.getYear() - birthDate.getYear();
     }
 
 }
