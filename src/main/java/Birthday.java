815b6b64cdf5aa41e09419af028d896b66d56779
Oskar Augustynek <oskar.augustynek@gmail.com>
2023-12-14 18:15:36 +0100

    visual refactor

--- src/main/java/Birthday.java
+++ src/main/java/Birthday.java
@@ -7,28 +7,28 @@ import java.util.Calendar;
 import java.util.Locale;
 
 public class Birthday {
-    public static void main(String[] args) {
-
-    }
-
     public static String getAge(String dateOfBirth){
         LocalDate localDateNow = LocalDate.now();
         LocalDate birthDate = LocalDate.parse(dateOfBirth);
         Period period = Period.between(birthDate, localDateNow);
-        System.out.println(localDateNow.getYear() - birthDate.getYear());
+        System.out.println("You are: " + (localDateNow.getYear() - birthDate.getYear()) + " years old!");
         return String.valueOf(localDateNow.getYear() - birthDate.getYear());
     }
 
     public static String getDayOfTheWeek(String dateOfBirth){
         LocalDate birthDate = LocalDate.parse(dateOfBirth);
-        System.out.println(birthDate.getDayOfWeek());
+        System.out.println("You were born on: " + birthDate.getDayOfWeek());
         return String.valueOf(birthDate.getDayOfWeek());
     }
 
     public static String getWeekOfTheYear(String dateOfBirth){
         LocalDate birthDate = LocalDate.parse(dateOfBirth);
         int weekOfTheYear = birthDate.get(WeekFields.of(Locale.GERMANY).weekOfYear());
-        System.out.println(weekOfTheYear);
+        System.out.println("On the: " + weekOfTheYear + " week of the year!");
         return String.valueOf(weekOfTheYear);
+    }
+
+    public static String getBrithdayInfo(String dateOfBirth){
+        return getAge(dateOfBirth) + " " + getDayOfTheWeek(dateOfBirth) + " " + getWeekOfTheYear(dateOfBirth);
     }
 }
