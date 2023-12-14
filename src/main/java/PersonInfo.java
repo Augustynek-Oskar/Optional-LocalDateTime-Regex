80fccf5b12a29832626842f341bc380b85ea84c4
Oskar Augustynek <oskar.augustynek@gmail.com>
2023-12-14 20:02:47 +0100

    phoneBookMap with V as PersonInfo

--- src/main/java/PersonInfo.java
+++ src/main/java/PersonInfo.java
@@ -4,24 +4,32 @@ public class PersonInfo {
     private String address;
 
     public PersonInfo(String fullName, String address) {
-        this.fullName = fullName;
         this.address = address;
+        this.fullName = fullName;
     }
 
     public PersonInfo(String fullName, String address, String phoneNumber) {
-        this.fullName = fullName;
         this.address = address;
+        this.phoneNumber = phoneNumber;
+        this.fullName = fullName;
     }
 
-    public String getFullName() {
-        return fullName;
+    @Override
+    public String toString() {
+        if (phoneNumber == null) return " {Full name :" + fullName + ", address: " + address + "}";
+        else return  " {Full name: " + fullName + ", address: " + address +  ", phone number: " + phoneNumber + "}";
     }
 
     public String getPhoneNumber() {
         return phoneNumber;
     }
 
+    public String getFullName() {
+        return fullName;
+    }
+
     public String getAddress() {
         return address;
     }
+
 }
--- src/main/java/PhoneBook.java
+++ src/main/java/PhoneBook.java
@@ -1,3 +1,16 @@
-public class PhoneBook {
+import java.util.HashMap;
+import java.util.Map;
 
+public class PhoneBook {
+    public static void main(String[] args) {
+
+        Map <String, PersonInfo> phoneBookMap = new HashMap<>();
+        phoneBookMap.put("Maria Wesołowska", new PersonInfo("Maria Wesołowska", "ul. Wesoła"));
+        phoneBookMap.put("Adam Nowak", new PersonInfo("Adam Nowak", "ul. Nowa", "623592359"));
+        phoneBookMap.put("Jan Kowalski", new PersonInfo("Jan Kowalski", "ul. Pierwsza"));
+
+        System.out.println(phoneBookMap.get("Adam Nowak"));
+        System.out.println(phoneBookMap.keySet());
+
+    }
 }
