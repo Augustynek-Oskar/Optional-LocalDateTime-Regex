53394143c1916e04f02cf3bf32bccf69ef5d59a3
Oskar Augustynek <oskar.augustynek@gmail.com>
2023-12-15 18:29:21 +0100

    Methods for adding and searching for personinfo

--- src/main/java/PhoneBook.java
+++ src/main/java/PhoneBook.java
@@ -2,15 +2,16 @@ import java.util.HashMap;
 import java.util.Map;
 
 public class PhoneBook {
-    public static void main(String[] args) {
-
-        Map <String, PersonInfo> phoneBookMap = new HashMap<>();
-        phoneBookMap.put("Maria Wesołowska", new PersonInfo("Maria Wesołowska", "ul. Wesoła"));
-        phoneBookMap.put("Adam Nowak", new PersonInfo("Adam Nowak", "ul. Nowa", "623592359"));
-        phoneBookMap.put("Jan Kowalski", new PersonInfo("Jan Kowalski", "ul. Pierwsza"));
-
-        System.out.println(phoneBookMap.get("Adam Nowak"));
-        System.out.println(phoneBookMap.keySet());
-
+    Map <String, PersonInfo> phoneBookMap = new HashMap<>();
+    public void addPerson(String fullname, String address, String phoneNum){
+        PersonInfo personInfo = new PersonInfo(fullname, address, phoneNum);
+        phoneBookMap.put(fullname, personInfo);
     }
+    public PersonInfo searchFor(String fullname) throws Exception {
+        if (phoneBookMap.containsKey(fullname)){
+            return phoneBookMap.get(fullname);
+        }
+        else throw new Exception("Nie znaleziono...");
+    }
+
 }
